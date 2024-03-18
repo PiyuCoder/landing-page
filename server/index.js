ba9ca58db573332cc0b53ba2cd0c38c96b407984
PiyuCodere <piyushrai284@gmail.comm>
2024-03-19 02:05:44 +0530

    first build

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 import { connectDb } from "./db/db.js";
 import subscriberRoute from "./routes/subscriber.js";
 
@@ -8,11 +10,19 @@ dotenv.config();
 connectDb();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use("/", express.static("build"));
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", subscriberRoute);
+
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "./build", "index.html"));
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}`)
